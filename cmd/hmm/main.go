35f5572eea0720a19ab2e112872c385fbbcb316f
dev <dev@obscurity.xyz>
2020-10-16 21:06:39 +0100

    cmd

--- cmd/hmm/main.go
+++ cmd/hmm/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/rssoftware/hmm"
 )
 
 func main() {
-	args := os.Args[:1]
-	fmt.Println(args)
+	args := os.Args[1:]
+	switch args[0] {
+	case "encrypt":
+		if len(args) == 1 {
+			fmt.Println("missing key")
+			return
+		}
+		if len(args) == 2 {
+			fmt.Println("missing data to encrypt")
+			return
+		}
+		encryptedData, err := hmm.Encrypt(args[1], args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(encryptedData)
+	case "decrypt":
+	}
 }
